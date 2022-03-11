0bc562dbc6be68b88fe288104abe64c11b4bda45
batyr0601 <batyr0601@gmail.com>
2022-03-11 14:16:58 -0600

    Initial Commit

---
+++ Diced/DicedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicedApp.swift
+//  Diced
+//
+//  Created by Batyr Zhangabylov on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DicedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
